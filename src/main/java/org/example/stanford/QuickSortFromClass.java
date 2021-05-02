d69f7a6b65d47e29e9b494e8b658450b54bba941
Rami Del Toro <rami.deltoro@gmail.com>
2021-05-02 14:57:53 -0400

    QuickSort Week 3 exercises

---
+++ src/main/java/org/example/stanford/QuickSortFromClass.java
@@ -0,0 +1,98 @@
+package org.example.stanford;
+
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class QuickSortFromClass {
+
+    enum PivotStrategy {
+        FIRST_ELEMENT,
+        LAST_ELEMENT,
+        MEDIAN_OF_THREE;
+    }
+
+    public int sort(int[] array, int leftIndex, int rightIndex, PivotStrategy pivotStrategy) {
+        int count = rightIndex -leftIndex;
+        if(leftIndex >= rightIndex) {
+            return 0;
+        }
+
+        int pivot = choosePivot(array,leftIndex,rightIndex,pivotStrategy);
+        swap(array,leftIndex,pivot);
+        int index = partition(array,leftIndex,rightIndex);
+
+        int countL = sort(array,leftIndex,index-1,pivotStrategy);
+        int countR = sort(array,index+1,rightIndex,pivotStrategy);
+
+        return count + countR + countL;
+    }
+
+
+
+
+
+    private int choosePivot(int[] array, int leftIndex, int rightIndex, PivotStrategy pivotStrategy) {
+        if (PivotStrategy.FIRST_ELEMENT == pivotStrategy) {
+
+        } else if(PivotStrategy.LAST_ELEMENT == pivotStrategy) {
+            swap(array,leftIndex,rightIndex);
+        } else if (PivotStrategy.MEDIAN_OF_THREE == pivotStrategy) {
+
+            int mid = ((rightIndex - leftIndex) / 2) + leftIndex;
+            int[] threeElementArray = new int[]{array[leftIndex],array[rightIndex],array[mid]};
+            int pivotIndex = 0;
+            Arrays.sort(threeElementArray);
+            if(threeElementArray[1] == array[leftIndex]) {
+                pivotIndex = leftIndex;
+            } else if(threeElementArray[1] == array[rightIndex]) {
+                pivotIndex = rightIndex;
+            } else {
+                pivotIndex = mid;
+            }
+
+            swap(array,leftIndex,pivotIndex);
+        }
+
+        return leftIndex;
+    }
+
+
+    private int partition(int[] array, int leftIndex, int rightIndex) {
+        int i = leftIndex + 1;
+        int pivot = array[leftIndex];
+
+        for(int j=leftIndex+1;j<rightIndex+1;j++) {
+            if(array[j] < pivot) {
+                swap(array,j,i);
+                i++;
+            }
+        }
+
+        swap(array,leftIndex,i-1);
+        return i-1;
+    }
+
+
+//    private int partition(int[] array, int first, int last) {
+//        System.out.println("first = "+first+" last = "+last);
+//        int pivotVal = array[first];
+//        int leftmark = first + 1;
+//
+//        for(int rightmark=first+1;rightmark<last+1;rightmark++) {
+//            if(array[rightmark] < pivotVal) {
+//                swap(array,leftmark,rightmark);
+//                leftmark++;
+//            }
+//        }
+//
+//        swap(array,leftmark-1,first);
+//        return leftmark-1;
+//    }
+
+
+    private void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
--- src/test/java/org/example/stanford/QuickSortTest.java
+++ src/test/java/org/example/stanford/QuickSortTest.java
@@ -2,14 +2,17 @@ package org.example.stanford;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class QuickSortTest {
 
     private final QuickSort quickSort = new QuickSort();
-
 
     @Test
     void sort() {
@@ -22,5 +25,52 @@ class QuickSortTest {
 
         assertArrayEquals(expectedArray,array);
 
+    }
+
+    @Test
+    void sort2() {
+
+        int[] array = new int[]{5,6,2,3,1,9};
+        int[] expectedArray = array.clone();
+        Arrays.sort(expectedArray);
+
+        new QuickSortFromClass().sort(array,0,array.length-1, QuickSortFromClass.PivotStrategy.FIRST_ELEMENT);
+
+        System.out.println(Arrays.toString(array));
+        assertArrayEquals(expectedArray,array);
+
+    }
+
+
+
+    @Test
+    void countComparison() throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("QuickSort.txt").getFile());
+        int[] array = new int[10000];
+        AtomicInteger atomicInteger = new AtomicInteger();
+        Files.lines(file.toPath())
+                .forEach(s -> {
+                    int number = Integer.valueOf(s);
+                    int index = atomicInteger.getAndIncrement();
+                    array[index] = number;
+                });
+
+        int[] array1 = array.clone();
+        int[] array2 = array.clone();
+        int[] array3 = array.clone();
+
+        assertEquals(162085,new QuickSortFromClass().sort(array1,0,array.length-1, QuickSortFromClass.PivotStrategy.FIRST_ELEMENT));
+        assertEquals(164123,new QuickSortFromClass().sort(array2,0,array.length-1, QuickSortFromClass.PivotStrategy.LAST_ELEMENT));
+        assertEquals(138382,new QuickSortFromClass().sort(array3,0,array.length-1, QuickSortFromClass.PivotStrategy.MEDIAN_OF_THREE));
+
+
+    }
+
+
+    @Test
+    void test() {
+        int i = 7 / 2;
+        System.out.println(i);
     }
 }
